119b5afb9cc5ec01a1d1979004779a3505da7a99
heyimmatt <mrmcfarland@gmail.com>
2020-06-27 12:04:57 -0700

    Generate a random message when the ball is clicked

--- src/EightBall.js
+++ src/EightBall.js
@@ -2,11 +2,18 @@ import React, {useState} from "react";
 import './EightBall.css'
 
 const EightBall = ({answers}) => {
+  const genRandom = () => Math.floor(Math.random() * answers.length);
+  const shakeEightBall = () => {
+    let num = genRandom();
+    setMessage(answers[num].msg);
+  }
+  const [message, setMessage] = useState('Think of a Question')
   return (
     <>
       <h1>Welcome to the Magic 8 Ball</h1>
-      <div className="EightBall">
-        <p><b>Think of a Question</b></p>
+      <h4>Click on the 8 Ball to Receive Advice</h4>
+      <div className="EightBall" onClick={shakeEightBall}>
+        <p><b>{message}</b></p>
       </div>
     </>
   )
