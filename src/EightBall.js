95262c656bf820f91123e4f771830b75effdeba7
heyimmatt <mrmcfarland@gmail.com>
2020-06-27 12:10:44 -0700

    Change bg color of the ball depending on the message

--- src/EightBall.js
+++ src/EightBall.js
@@ -5,14 +5,16 @@ const EightBall = ({answers}) => {
   const genRandom = () => Math.floor(Math.random() * answers.length);
   const shakeEightBall = () => {
     let num = genRandom();
+    setBgColor(answers[num].color);
     setMessage(answers[num].msg);
   }
   const [message, setMessage] = useState('Think of a Question')
+  const [bgColor, setBgColor] = useState('black')
   return (
     <>
       <h1>Welcome to the Magic 8 Ball</h1>
       <h4>Click on the 8 Ball to Receive Advice</h4>
-      <div className="EightBall" onClick={shakeEightBall}>
+      <div className="EightBall" onClick={shakeEightBall} style={{backgroundColor: bgColor}}>
         <p><b>{message}</b></p>
       </div>
     </>
