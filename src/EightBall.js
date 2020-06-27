2ea1ba28b847296fc2eadcd8ce6a1dd02c363425
heyimmatt <mrmcfarland@gmail.com>
2020-06-27 12:39:50 -0700

    Keep track of the number of times a color appears until the page is refreshed

--- src/EightBall.js
+++ src/EightBall.js
@@ -5,15 +5,28 @@ const EightBall = ({answers}) => {
   const genRandom = () => Math.floor(Math.random() * answers.length);
   const shakeEightBall = () => {
     let num = genRandom();
-    setBgColor(answers[num].color);
+    let color = answers[num].color;
+    setBgColor(color);
     setMessage(answers[num].msg);
+    if (color === 'green') {
+      setGreenCount(greenCount + 1);
+    }
+    else if (color === 'goldenrod') {
+      setYellowCount(yellowCount + 1);
+    }
+    else if (color === 'red') {
+      setRedCount(redCount + 1);
+    }
   }
   const reset = () => {
     setBgColor('black');
     setMessage('Think of a Question');
   }
-  const [message, setMessage] = useState('Think of a Question')
-  const [bgColor, setBgColor] = useState('black')
+  const [message, setMessage] = useState('Think of a Question');
+  const [bgColor, setBgColor] = useState('black');
+  const [greenCount, setGreenCount] = useState(0);
+  const [yellowCount, setYellowCount] = useState(0);
+  const [redCount, setRedCount] = useState(0);
   return (
     <>
       <h1>Welcome to the Magic 8 Ball</h1>
@@ -22,6 +35,8 @@ const EightBall = ({answers}) => {
         <p><b>{message}</b></p>
       </div>
       <button onClick={reset}>Reset</button>
+      <p><b>Color Count</b></p>
+      <p>Green: {greenCount} | Yellow: {yellowCount} | Red: {redCount}</p>
     </>
   )
 }
